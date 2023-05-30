e76a4bfc540db3b415aa70b0cd7a65c7e4cc7afd
Mark Wilkinson <markw@illuminae.com>
2023-05-30 08:40:23 +0200

    beginning of FDP mate

---
+++ lib/fdp_mate.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fdp_mate/version"
+
+module FdpMate
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/fdp_mate_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FdpMate do
+  it "has a version number" do
+    expect(FdpMate::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
